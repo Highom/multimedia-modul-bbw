3c3fec05b99bd17deb3032c03d853cda0c2d33cd
Highom <highomlp@gmail.com>
2021-04-19 18:08:02 +0200

    refactored the imgdiv hover

--- script.js
+++ script.js
@@ -55,10 +55,10 @@ imagesVertical.forEach(function(image, i) {
     div.className = "col-sm-4 col-xs-12 flex-item card";
     div.id = "imgBeforeAfter" + (i+1);
     imgsdiv.className = "imgrow"
-    imgdiv1.className = "imgdivVertical";
-    imgdiv2.className = "imgdivVertical";
-    img1.className = "img-fluid";
-    img2.className = "img-fluid";
+    imgdiv1.className = "imgdiv";
+    imgdiv2.className = "imgdiv";
+    img1.className = "img-fluid img-blur";
+    img2.className = "img-fluid img-blur";
     img1.src = imgFolder + image[0] + "-1.jpg";
     img2.src = imgFolder + image[0] + "-2.jpg";
     text.textContent = image[1];
