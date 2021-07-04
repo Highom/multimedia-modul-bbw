7f597be5239f72b82f7555a2eea2f946c2af500c
Highom <highomlp@gmail.com>
2021-07-04 13:22:21 +0200

    switched to use sharp

--- firebaseFunctions/functions/index.js
+++ firebaseFunctions/functions/index.js
@@ -18,8 +18,8 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const mkdirp = require('mkdirp');
-const spawn = require('child-process-promise').spawn;
 const path = require('path');
+const sharp = require('sharp');
 const os = require('os');
 const fs = require('fs');
 
@@ -32,7 +32,7 @@ const JPEG_EXTENSION = '.jpg';
  * When an image is uploaded in the Storage bucket it is converted to JPEG automatically using
  * ImageMagick.
  */
-exports.imageToJPG = functions.region('europe-west1').storage.object().onFinalize(async (object) => {
+exports.convertImageAfterUpload = functions.region('europe-west1').storage.object().onFinalize(async (object) => {
   const filePath = object.name;
   const baseFileName = path.basename(filePath, path.extname(filePath));
   const fileDir = path.dirname(filePath);
@@ -41,15 +41,11 @@ exports.imageToJPG = functions.region('europe-west1').storage.object().onFinaliz
   const tempLocalDir = path.dirname(tempLocalFile);
   const tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath);
 
+  sharp.cache(false);
+
   // Exit if this is triggered on a file that is not an image.
   if (!object.contentType.startsWith('image/')) {
     functions.logger.log('This is not an image.');
-    return null;
-  }
-
-  // Exit if the image is already a JPEG.
-  if (object.contentType.startsWith('image/jpeg')) {
-    functions.logger.log('Already a JPEG.');
     return null;
   }
 
@@ -59,8 +55,16 @@ exports.imageToJPG = functions.region('europe-west1').storage.object().onFinaliz
   // Download file from bucket.
   await bucket.file(filePath).download({destination: tempLocalFile});
   functions.logger.log('The file has been downloaded to', tempLocalFile);
-  // Convert the image to JPEG using ImageMagick.
-  await spawn('convert', [tempLocalFile, tempLocalJPEGFile]);
+  let buffer = await sharp(tempLocalFile)
+  .resize(858,480)
+  .toFormat('jpg')
+  .withMetadata()
+  .toBuffer().catch(err =>{
+    functions.logger.error('Conversion failed. Error:',err);
+  });
+  await sharp(buffer).toFile(tempLocalJPEGFile).catch(err =>{
+    functions.logger.error('Saving new File failed. Error:',err);
+  });
   functions.logger.log('JPEG image created at', tempLocalJPEGFile);
   // Uploading the JPEG image.
   await bucket.upload(tempLocalJPEGFile, {destination: JPEGFilePath});
